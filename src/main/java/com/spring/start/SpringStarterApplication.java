57de5b0ff1e021f7c5bc4b2f3dc040a23bc734a0
true-false-try <6mateo6ch@gmail.com>
2021-12-21 10:59:00 +0200

    Were inserted a couple of users

--- src/main/java/com/spring/start/SpringStarterApplication.java
+++ src/main/java/com/spring/start/SpringStarterApplication.java
@@ -1,13 +1,39 @@
 package com.spring.start;
 
+import com.spring.start.model.Role;
+import com.spring.start.model.User;
+import com.spring.start.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import java.util.Set;
+
 @SpringBootApplication
-public class SpringStarterApplication {
+@AllArgsConstructor
+public class SpringStarterApplication implements ApplicationRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringStarterApplication.class, args);
+    private final UserRepository userRepository;
+
+    public static void main(String[] args) {SpringApplication.run(SpringStarterApplication.class, args);}
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        userRepository.save(new User("yevhenii@gmail.com",
+                                     "Yevhenii",
+                                     "Volovitskyi" ,
+                                     "qwerty1234",
+                                      Set.of(Role.ROLE_ADMIN)));
+
+        userRepository.save(new User("alex@gmail.com",
+                                     "Alex",
+                                     "Rolman" ,
+                                     "qwerty12344321",
+                                     Set.of(Role.ROLE_USER)));
+
+        System.out.println(userRepository.findAll());
     }
-
 }
--- src/main/java/com/spring/start/model/Role.java
+++ src/main/java/com/spring/start/model/Role.java
@@ -1,2 +1,6 @@
-package com.spring.start.model;public enum Role {
+package com.spring.start.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
 }
